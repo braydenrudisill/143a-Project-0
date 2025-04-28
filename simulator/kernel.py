556f4d69af302646b5fa33e27c65d3cdfe192d06
Brayden Rudisill <braydenrudisill@gmail.com>
2025-04-28 14:33:43 -0700

    Dataclasses and cleanup

--- simulator/kernel.py
+++ simulator/kernel.py
@@ -2,70 +2,69 @@
 # Group id: 3
 # Members: Brayden Rudisill, Rhea Jethvani
 
-
-
 from collections import deque
+from dataclasses import dataclass, field
 
-# PID is just an integer, but it is used to make it clear when a integer is expected to be a valid PID.
+
 PID = int
+"""PID is just an integer.
+- It is used to make it clear when a integer is expected to be a valid PID.
+"""
 
-# This class represents the PCB of processes.
-# It is only here for your convinience and can be modified however you see fit.
+
+@dataclass
 class PCB:
+    """Represents the PCB of processes.
+    It is only here for your convenience and can be modified however you see fit.
+    """
     pid: PID
 
-    def __init__(self, pid: PID):
-        self.pid = pid
 
-# This class represents the Kernel of the simulation.
-# The simulator will create an instance of this object and use it to respond to syscalls and interrupts.
-# DO NOT modify the name of this class or remove it.
+@dataclass
 class Kernel:
+    """Represents the Kernel of the simulation.
+    - The simulator will create an instance of this object and use it to respond to
+        syscalls and interrupts.
+    - DO NOT modify the name of this class or remove it."""
     scheduling_algorithm: str
-    ready_queue: deque[PCB]
-    waiting_queue: deque[PCB]
-    running: PCB
-    idle_pcb: PCB
+    ready_queue: deque[PCB] = field(default_factory=deque)
+    waiting_queue: deque[PCB] = field(default_factory=deque)
+    running: PCB = 0
+    idle_pcb: PCB = 0
 
-    # Called before the simulation begins.
-    # Use this method to initilize any variables you need throughout the simulation.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
-    def __init__(self, scheduling_algorithm: str):
-        self.scheduling_algorithm = scheduling_algorithm
-        self.ready_queue = deque()
-        self.waiting_queue = deque()
-        self.idle_pcb = PCB(0)
-        self.running = self.idle_pcb
-
-    # This method is triggered every time a new process has arrived.
-    # new_process is this process's PID.
-    # priority is the priority of new_process.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def new_process_arrived(self, new_process: PID, priority: int) -> PID:
+        """Triggered every time a new process has arrived.
+        - new_process is this process's PID.
+        - priority is the priority of new_process.
+        - DO NOT rename or delete this method. DO NOT change its arguments.
+        """
+        # TODO: Implement
         return self.running.pid
 
-    # This method is triggered every time the current process performs an exit syscall.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_exit(self) -> PID:
+        """Triggered every time the current process performs an exit syscall.
+        - DO NOT rename or delete this method. DO NOT change its arguments.
+        """
+        # TODO: Implement
         return self.running.pid
 
-    # This method is triggered when the currently running process requests to change its priority.
-    # DO NOT rename or delete this method. DO NOT change its arguments.
     def syscall_set_priority(self, new_priority: int) -> PID:
+        """Triggered when the currently running process requests to change its priority.
+        - DO NOT rename or delete this method. DO NOT change its arguments.
+        """
+        # TODO: Implement
         return self.running.pid
 
-
-    # This is where you can select the next process to run.
-    # This method is not directly called by the simulator and is purely for your convinience.
-    # Feel free to modify this method as you see fit.
-    # It is not required to actually use this method but it is recommended.
     def choose_next_process(self):
+        """This is where you can select the next process to run.
+        - Not directly called by the simulator and is purely for your convenience.
+        - Feel free to modify this method as you see fit.
+        - It is not required to actually use this method, but it is recommended.
+        """
         if len(self.ready_queue) == 0:
-                return self.idle_pcb
+            return self.idle_pcb
         
         if self.scheduling_algorithm == "FCFS":
             self.running = self.idle_pcb
         elif self.scheduling_algorithm == "Priority":
             self.running = self.idle_pcb
-        
-
